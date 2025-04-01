f42eccd86f3de595cfb1cc8e358510054968fcc1
Vidunasri57 <vidusaro@gmail.com>
2025-04-01 12:08:15 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,23 @@
+//remove repeated elements in a array
+function removeDuplicates(arr) {
+    let uniqueArray = [];
+    for (let num of arr) {
+        if (!uniqueArray.includes(num)) {
+            uniqueArray.push(num);
+        }
+    }
+    return uniqueArray;
+}
+
+let numbers = [5, 7, 7, 8, 9, 5, 10];
+let uniqueNumbers = removeDuplicates(numbers);
+console.log(uniqueNumbers); 
+
+//palindrome
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+console.log(isPalindrome("racecar")); 
+console.log(isPalindrome("hello"));   
